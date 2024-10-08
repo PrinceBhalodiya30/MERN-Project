a8fbcc8f9438b8a14efe5dddddda93665ed85efd
PrinceBhalodiya30 <146628141+PrinceBhalodiya30@users.noreply.github.com>
2024-10-08 13:50:26 +0530

    Delete Project_node directory

--- Project_node/employeeSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const empSchema = new mongoose.Schema({
-  EmployeeId: Number,
-  Name: String,
-  Department: String,
-  Position: String,
-  Salary: Number
-});
-module.exports = mongoose.model("Employee", empSchema);
